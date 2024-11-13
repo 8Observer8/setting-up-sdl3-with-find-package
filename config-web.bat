25a7e06ab65e54841513579a35fecfc7d87f7010
Ivan Enzhaev <8observer8@gmail.com>
2024-11-13 20:17:11 +0400

    Initial commit

---
+++ config-web.bat
@@ -0,0 +1,1 @@
+emcmake cmake -G "MinGW Makefiles" -S . -B dist/web -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3 -DCMAKE_BUILD_TYPE=Debug
---
+++ config-win.bat
@@ -0,0 +1,1 @@
+cmake -G "MinGW Makefiles" -S . -B dist/win -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 -DCMAKE_BUILD_TYPE=Debug
